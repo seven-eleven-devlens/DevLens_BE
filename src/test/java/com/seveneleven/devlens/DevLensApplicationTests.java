1c58a70119b2c03d6757143eee8aba3d78d84ba6
K-KY <kyuyoungk@naver.com>
2024-12-31 02:42:10 +0900

    Setting : Test 디렉토리 추가

---
+++ src/test/java/com/seveneleven/devlens/DevLensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seveneleven.devlens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevLensApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
