f97f26a1111eb5b88f45483d049839b1a8da7a39
taekminKwon <rnjsxorals1@gmail.com>
2025-01-01 15:03:40 +0900

    Feat : 회사 등록 구현 - 회사 등록 Api 구현 - 예외처리(CompanyDuplicatedException.java) : 중복회사 조회 - 중복회사 에러코드(COMPANY_DUPLICATED_NUMBER) 추가 : 임의로 1000 부여, 추후 변경 예정

--- src/main/java/com/seveneleven/devlens/domain/admin/controller/CompanyController.java
+++ src/main/java/com/seveneleven/devlens/domain/admin/controller/CompanyController.java
@@ -2,9 +2,11 @@ package com.seveneleven.devlens.domain.admin.controller;
 
 import com.seveneleven.devlens.domain.admin.dto.CompanyDto;
 import com.seveneleven.devlens.domain.admin.service.CompanyServiceImpl;
+import com.seveneleven.devlens.domain.member.entity.Company;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,7 +22,10 @@ public class CompanyController {
     private final CompanyServiceImpl companyService;
 
     @PostMapping("/create")
-    public void createCompany(@Valid @RequestBody CompanyDto companyDto) {
-        companyService.createCompany(companyDto);
+    public ResponseEntity<Company> createCompany(@Valid @RequestBody CompanyDto companyDto) {
+        var company = companyService.createCompany(companyDto);
+        return ResponseEntity.ok()
+                .body(company);
     }
+
 }
