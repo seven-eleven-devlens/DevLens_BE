0cd6e25a8fa54d8ead3502d35749074c61aee080
taekminKwon <rnjsxorals1@gmail.com>
2025-01-01 15:22:06 +0900

    Test : 회사 등록 Api 테스트 - 등록 : 회사 추가하기 기능 - 예외처리 : 중복 시 예외처리 발생 확인

---
+++ src/test/java/com/seveneleven/devlens/domain/admin/service/CompanyServiceImplTest.java
@@ -0,0 +1,93 @@
+package com.seveneleven.devlens.domain.admin.service;
+
+import com.seveneleven.devlens.domain.admin.dto.CompanyDto;
+import com.seveneleven.devlens.domain.admin.exception.CompanyDuplicatedException;
+import com.seveneleven.devlens.domain.admin.repository.CompanyRepository;
+import com.seveneleven.devlens.domain.member.entity.Company;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class CompanyServiceImplTest {
+    @Mock
+    private CompanyRepository companyRepository;
+    @InjectMocks
+    private CompanyServiceImpl companyService;
+
+    @DisplayName("회사 추가하기 기능")
+    @Test
+    void createCompany() {
+        var companyName = "companyName";
+        var representativeName = "representativeName";
+        var representativeEmail = "representativeEmail";
+        var representativeContact = "representativeContact";
+        var address = "address";
+        var businessType = "businessType";
+        var businessRegistrationNumber = "businessRegistrationNumber";
+        var representativeImageExists = false;
+        var activeStatus = true;
+
+        when(companyRepository.save(any(Company.class)))
+                .thenAnswer(invocation -> {
+                            var e = (Company) invocation.getArgument(0);
+                            e.setCompanyName(companyName);
+                            e.setRepresentativeName(representativeName);
+                            e.setRepresentativeEmail(representativeEmail);
+                            e.setRepresentativeContact(representativeContact);
+                            e.setAddress(address);
+                            e.setBusinessType(businessType);
+                            e.setBusinessRegistrationNumber(businessRegistrationNumber);
+                            e.setRepresentativeImageExists(representativeImageExists);
+                            e.setActiveStatus(activeStatus);
+                            return e;
+                        }
+                );
+        CompanyDto companyDto = new CompanyDto(companyName, representativeName, representativeEmail, representativeContact, address, businessType, businessRegistrationNumber);
+
+        var actual = companyService.createCompany(companyDto);
+        verify(companyRepository, times(1)).save(any(Company.class));
+        assertEquals(companyName, actual.getCompanyName());
+        assertEquals(representativeName, actual.getRepresentativeName());
+        assertEquals(representativeEmail, actual.getRepresentativeEmail());
+        assertEquals(representativeContact, actual.getRepresentativeContact());
+        assertEquals(address, actual.getAddress());
+        assertEquals(businessType, actual.getBusinessType());
+        assertEquals(businessRegistrationNumber, actual.getBusinessRegistrationNumber());
+        assertEquals(representativeImageExists, actual.getRepresentativeImageExists());
+        assertEquals(activeStatus, actual.getActiveStatus());
+    }
+
+    @DisplayName("중복 시 예외처리 발생 확인")
+    @Test
+    void createCompany_shouldThrowException() {
+        var companyName = "companyName";
+        var representativeName = "representativeName";
+        var representativeEmail = "representativeEmail";
+        var representativeContact = "representativeContact";
+        var address = "address";
+        var businessType = "businessType";
+        // Given
+        String duplicatedBusinessNumber = "1234567890";
+        CompanyDto companyDto = new CompanyDto(companyName, representativeName,  representativeContact, representativeEmail, address, businessType, duplicatedBusinessNumber);
+        Company company = companyDto.createCompanyEntity();
+
+        // Mock: 이미 등록된 회사가 있는 경우를 시뮬레이션
+        when(companyRepository.findByBusinessRegistrationNumber(duplicatedBusinessNumber))
+                .thenReturn(company);
+
+        // When & Then
+        assertThrows(CompanyDuplicatedException.class,
+                () -> companyService.createCompany(companyDto));
+
+        // Verify: repository 호출이 제대로 이루어졌는지 확인
+        verify(companyRepository, times(1)).findByBusinessRegistrationNumber(duplicatedBusinessNumber);
+        verify(companyRepository, never()).save(any(Company.class)); // save는 호출되지 않아야 함
+    }
+}
