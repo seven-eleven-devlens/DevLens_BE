a207cd47f5f577d6a3d1b3b5fd5b8670713a94dd
taekminKwon <rnjsxorals1@gmail.com>
2025-01-03 16:37:45 +0900

    Update : 페이징 반환값 List에서 PaginatedResponse로 수정

---
+++ src/main/java/com/seveneleven/devlens/domain/admin/dto/PaginatedResponse.java
@@ -0,0 +1,27 @@
+package com.seveneleven.devlens.domain.admin.dto;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PaginatedResponse<T> {
+    private List<T> content;
+    private int pageNumber;
+    private int pageSize;
+    private long totalElements;
+    private int totalPages;
+    private boolean last;
+
+    public PaginatedResponse(List<T> content, int pageNumber, int pageSize, long totalElements, int totalPages, boolean last) {
+        this.content = content;
+        this.pageNumber = pageNumber;
+        this.pageSize = pageSize;
+        this.totalElements = totalElements;
+        this.totalPages = totalPages;
+        this.last = last;
+    }
+}
