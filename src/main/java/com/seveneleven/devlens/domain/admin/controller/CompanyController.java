70fbac11cb869165a8ece784f416cd71e84034c9
taekminKwon <rnjsxorals1@gmail.com>
2025-01-03 15:55:43 +0900

    Update : PR 피드백 기반 Company Dto 및 Entity 수정

--- src/main/java/com/seveneleven/devlens/domain/admin/controller/CompanyController.java
+++ src/main/java/com/seveneleven/devlens/domain/admin/controller/CompanyController.java
@@ -1,20 +1,19 @@
 package com.seveneleven.devlens.domain.admin.controller;
 
-import com.seveneleven.devlens.domain.admin.model.CompanyDto;
+import com.seveneleven.devlens.domain.admin.db.CompanyResponseConverter;
+import com.seveneleven.devlens.domain.admin.dto.CompanyDto;
 import com.seveneleven.devlens.domain.admin.service.CompanyCreateService;
 import com.seveneleven.devlens.domain.admin.service.CompanyReadService;
 import com.seveneleven.devlens.global.response.APIResponse;
 import com.seveneleven.devlens.global.response.SuccessCode;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Slf4j
 @Validated
 @RestController
 @RequiredArgsConstructor
@@ -22,23 +21,24 @@ import java.util.List;
 public class CompanyController {
     private final CompanyCreateService companyCreateService;
     private final CompanyReadService companyReadService;
+    private final CompanyResponseConverter companyResponseConverter;
 
     @PostMapping("/create")
-    public APIResponse<CompanyDto> createCompany(@Valid @RequestBody CompanyDto companyDto) {
-        CompanyDto company = companyCreateService.createCompany(companyDto);
-        return APIResponse.create(company);
+    public APIResponse<CompanyDto.CompanyResponse> createCompany(@Valid @RequestBody CompanyDto.CompanyRequest companyRequest) {
+        var company = companyCreateService.createCompany(companyRequest);
+        return APIResponse.success(company);
     }
 
     @GetMapping("/{companyId}")
-    public APIResponse<CompanyDto> readCompany(@PathVariable Long companyId) {
-        var company = companyReadService.getCompanyDto(companyId);
+    public APIResponse<CompanyDto.CompanyResponse> readCompany(@PathVariable Long companyId) {
+        var company = companyReadService.getCompanyResponse(companyId);
         return APIResponse.success(SuccessCode.OK, "회사 조회가 완료되었습니다.",company);
     }
 
     @GetMapping("")
-    public APIResponse<List<CompanyDto>> readAllCompanies(@RequestParam(value = "page", defaultValue = "0") int page) {
-        Page<CompanyDto> paging = this.companyReadService.getListOfCompanies(page);
-        return APIResponse.success(SuccessCode.OK, paging.getContent());
+    public APIResponse<List<CompanyDto.CompanyResponse>> readAllCompanies(@RequestParam(value = "page", defaultValue = "0") int page) {
+        Page<CompanyDto.CompanyResponse> paging = companyReadService.getListOfCompanies(page);
+        return APIResponse.success(paging.getContent());
     }
 
 }
