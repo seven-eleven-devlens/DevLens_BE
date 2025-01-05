10f68bbe48177cd6612677c48b7dfeee61bb79ee
HiSeungmin <osminosmin7180@gmail.com>
2025-01-05 14:28:50 +0900

    Chore: 약관 이력 테이블 상태 컬럼 추가
    - 약관 동의 이력 테이블 상태 컬럼 제거
    - 약관 이력 테이블 상태 컬럼, 상태 변경 메서드 추가
    
    close: #32

--- src/main/java/com/seveneleven/devlens/domain/member/entity/Term.java
+++ src/main/java/com/seveneleven/devlens/domain/member/entity/Term.java
@@ -1,9 +1,11 @@
 package com.seveneleven.devlens.domain.member.entity;
 
+import com.seveneleven.devlens.domain.member.constant.TermsStatus;
 import com.seveneleven.devlens.domain.member.constant.YN;
 import com.seveneleven.devlens.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
+import org.joda.time.DateTime;
 
 @Entity
 @Getter
@@ -26,18 +28,26 @@ public class Term extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private YN isRequired; // 필수 여부
 
+    @Column(name = "expiration_date")
+    private DateTime expiration_date; // 만료 일자
+
+    @Column(name = "status", length = 50)
+    @Enumerated(EnumType.STRING)
+    private TermsStatus status; // 상태
+
 
     // 생성 메서드
-    public static Term createTerm(String title, String content, YN isRequired) {
+    public static Term createTerm(String title, String content, YN isRequired, TermsStatus status) {
         Term term = new Term();
         term.title      = title;
         term.content    = content;
         term.isRequired = isRequired != null? isRequired : YN.N;
+        term.status     = status;
         return term;
     }
 
-    // 삭제 메서드
-    public void delete() {
-
+    // 상태 변경 메서드
+    public void updateStatus(TermsStatus status) {
+        this.status = status;
     }
 }
