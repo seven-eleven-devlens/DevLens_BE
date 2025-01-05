5bac9a1b775e12b19730dc0378598be56f977620
taekminKwon <rnjsxorals1@gmail.com>
2025-01-05 20:21:27 +0900

    미사용 import문 삭제

--- src/main/java/com/seveneleven/devlens/domain/admin/db/CompanyRepository.java
+++ src/main/java/com/seveneleven/devlens/domain/admin/db/CompanyRepository.java
@@ -1,13 +1,10 @@
 package com.seveneleven.devlens.domain.admin.db;
 
 import com.seveneleven.devlens.domain.member.entity.Company;
-import lombok.NonNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
-import javax.swing.text.html.Option;
 import java.util.Optional;
 
 public interface CompanyRepository extends JpaRepository<Company, Long> {
