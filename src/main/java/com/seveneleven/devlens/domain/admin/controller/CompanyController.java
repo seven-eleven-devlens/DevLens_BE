f11d1486d0493bf20a8052b61a7e0494dbecd3a4
taekminKwon <rnjsxorals1@gmail.com>
2025-01-05 13:51:40 +0900

    Update : POST URI /create to ""로 변경, GET URI /list 로 변경

--- src/main/java/com/seveneleven/devlens/domain/admin/controller/CompanyController.java
+++ src/main/java/com/seveneleven/devlens/domain/admin/controller/CompanyController.java
@@ -5,7 +5,6 @@ import com.seveneleven.devlens.domain.admin.dto.PaginatedResponse;
 import com.seveneleven.devlens.domain.admin.service.CompanyCreateService;
 import com.seveneleven.devlens.domain.admin.service.CompanyReadService;
 import com.seveneleven.devlens.global.response.APIResponse;
-import com.seveneleven.devlens.global.response.SuccessCode;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -23,7 +22,7 @@ public class CompanyController {
         함수명 : createCompany
         목적 : 회사 생성하여 db에 저장
      */
-    @PostMapping("/create")
+    @PostMapping("")
     public APIResponse<CompanyDto.CompanyResponse> createCompany(@Valid @RequestBody CompanyDto.CompanyRequest companyRequest) {
         var company = companyCreateService.createCompany(companyRequest);
         return APIResponse.success(company);
@@ -33,18 +32,18 @@ public class CompanyController {
         함수명 : readCompany
         목적 : 회사 상세 정보 조회
      */
-    @GetMapping("/{companyId}")
+    @GetMapping("/list/{companyId}")
     public APIResponse<CompanyDto.CompanyResponse> readCompany(@PathVariable Long companyId) {
         var company = companyReadService.getCompanyResponse(companyId);
-        return APIResponse.success(SuccessCode.OK, "회사 조회가 완료되었습니다.",company);
+        return APIResponse.success(company);
     }
 
     /*
         함수명 : readAllCompanies
         목적 : 회사 페이지 조회
      */
-    @GetMapping("")
-    public APIResponse<PaginatedResponse<CompanyDto.CompanyResponse>> readAllCompanies(@RequestParam(value = "page", defaultValue = "0") int page) {
+    @GetMapping("/list")
+    public APIResponse<PaginatedResponse<CompanyDto.CompanyResponse>> readCompanyPage(@RequestParam(value = "page", defaultValue = "0") int page) {
         Page<CompanyDto.CompanyResponse> paging = companyReadService.getListOfCompanies(page);
         PaginatedResponse<CompanyDto.CompanyResponse> response = new PaginatedResponse<>(
                 paging.getContent(),
