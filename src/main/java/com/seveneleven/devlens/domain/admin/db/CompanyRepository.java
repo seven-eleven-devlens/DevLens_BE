55a1da84da238e7e9b8bf4999ec2ca8c09f51cdc
taekminKwon <rnjsxorals1@gmail.com>
2025-01-06 14:30:48 +0900

    update : getListOfCompanies 수정

--- src/main/java/com/seveneleven/devlens/domain/admin/db/CompanyRepository.java
+++ src/main/java/com/seveneleven/devlens/domain/admin/db/CompanyRepository.java
@@ -1,14 +1,13 @@
 package com.seveneleven.devlens.domain.admin.db;
 
+import com.seveneleven.devlens.domain.member.constant.YN;
 import com.seveneleven.devlens.domain.member.entity.Company;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
 
 public interface CompanyRepository extends JpaRepository<Company, Long> {
     Optional<Company> findByBusinessRegistrationNumber(String businessRegistrationNumber);
-    @Query("SELECT c FROM Company c")
-    Page<Company> findPage(Pageable pageable);
+    Page<Company> findByIsActive(Pageable pageable, YN isActive);
 }
