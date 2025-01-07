6895602d15b9b2c0b0c180e86617e1d93d699256
taekminKwon <rnjsxorals1@gmail.com>
2025-01-07 13:23:21 +0900

    update : deactivate, activate 구문 삭제

--- src/main/java/com/seveneleven/devlens/domain/admin/db/CompanyRepository.java
+++ src/main/java/com/seveneleven/devlens/domain/admin/db/CompanyRepository.java
@@ -5,10 +5,6 @@ import com.seveneleven.devlens.domain.member.entity.Company;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -16,12 +12,4 @@ public interface CompanyRepository extends JpaRepository<Company, Long> {
     Optional<Company> findByBusinessRegistrationNumberAndIsActive(String businessRegistrationNumber, YN isActive);
     Optional<Company> findByIdAndIsActive(Long id, YN isActive);
     Page<Company> findByIsActive(Pageable pageable, YN isActive);
-    @Transactional
-    @Modifying
-    @Query("UPDATE Company c SET c.isActive = 'N' WHERE c.id = :id")
-    void deactivateCompany(@Param("id") Long id);
-    @Transactional
-    @Modifying
-    @Query("UPDATE Company c SET c.isActive = 'Y' WHERE c.id = :id")
-    void activateCompany(@Param("id") Long id);
 }
