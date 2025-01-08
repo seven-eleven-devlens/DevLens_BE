9e5f97b282eb62686a8c897c1fd9ee9c38b46561
HiSeungmin <osminosmin7180@gmail.com>
2025-01-08 19:19:05 +0900

    Feat: 회원 목록 조회 기능

--- src/main/java/com/seveneleven/devlens/domain/admin/controller/MemberMgmtController.java
+++ src/main/java/com/seveneleven/devlens/domain/admin/controller/MemberMgmtController.java
@@ -1,60 +1,65 @@
 package com.seveneleven.devlens.domain.admin.controller;
 
+import com.seveneleven.devlens.domain.admin.dto.MemberDto;
+import com.seveneleven.devlens.domain.admin.service.MemberMgmtService;
+import com.seveneleven.devlens.global.response.APIResponse;
+import com.seveneleven.devlens.global.response.SuccessCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/admin")
-// @PreAuthorize("hasRole('ADMIN')")
+// @PreAuthorize("hasRole('ADMIN')")  // 관리자 권한만 접근 가능하도록 설정
 public class MemberMgmtController {
 
-    // 관리자 권한만 접근 가능하도록 설정
-
+    private final MemberMgmtService memberMgmtService;
 
     // 회원 목록 조회
     @GetMapping("/member")
-    public ResponseEntity<?> memberList() {
+    public ResponseEntity<APIResponse<Object>> memberList() {
 
-        // 관리자 체크
+        List<MemberDto> memberDtoList = memberMgmtService.getAllMembers();
+        System.out.println(memberDtoList.toString());
 
-
-
-        return null;
+        return ResponseEntity.status(SuccessCode.OK.getStatus())
+                .body(APIResponse.success(SuccessCode.OK, "회원 목록을 성공적으로 조회했습니다.", memberDtoList));
     }
 
     // 회원 상세 조회
     @GetMapping("/member/{memberId}")
-    public ResponseEntity<?> memberDetail() {
+    public ResponseEntity<APIResponse<SuccessCode>> memberDetail() {
 
         return null;
     }
 
     // 계정 생성
     @PostMapping("/member/")
-    public ResponseEntity<?> createMember() {
+    public ResponseEntity<APIResponse<SuccessCode>> createMember() {
 
         return null;
     }
 
     // 계정 수정
     @PutMapping("/member/{memberId}")
-    public ResponseEntity<?> updateMember() {
+    public ResponseEntity<APIResponse<SuccessCode>>updateMember() {
 
         return null;
     }
 
     // 계정 삭제
     @DeleteMapping("/member/{memberId}")
-    public ResponseEntity<?> deleteMember() {
+    public ResponseEntity<APIResponse<SuccessCode>> deleteMember() {
 
         return null;
     }
 
     // 회원 비밀번호 초기화
     @PutMapping("/member/{memberId}/reset-password")
-    public ResponseEntity<?> resetPwd() {
+    public ResponseEntity<APIResponse<SuccessCode>> resetPwd() {
 
         return null;
     }
--- src/main/java/com/seveneleven/devlens/domain/admin/service/MemberMgmtService.java
+++ src/main/java/com/seveneleven/devlens/domain/admin/service/MemberMgmtService.java
@@ -1,4 +1,28 @@
 package com.seveneleven.devlens.domain.admin.service;
 
+
+import com.seveneleven.devlens.domain.admin.dto.MemberDto;
+import com.seveneleven.devlens.domain.member.entity.Member;
+import com.seveneleven.devlens.domain.member.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Service
+@AllArgsConstructor
 public class MemberMgmtService {
+
+    private final MemberRepository memberRepository;
+
+    public List<MemberDto> getAllMembers() {
+
+        List<Member> members = memberRepository.findAll();
+
+        return members.stream()
+                .map(MemberDto::fromEntity)
+                .collect(Collectors.toList());
+    }
 }
