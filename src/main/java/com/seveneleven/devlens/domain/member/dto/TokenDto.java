eb2c401ca2472b2bac35fa9db186ded4521ef2ec
HiSeungmin <osminosmin7180@gmail.com>
2025-01-08 10:27:04 +0900

    Chore: remove setter in token dto

--- src/main/java/com/seveneleven/devlens/domain/member/dto/TokenDto.java
+++ src/main/java/com/seveneleven/devlens/domain/member/dto/TokenDto.java
@@ -3,11 +3,9 @@ package com.seveneleven.devlens.domain.member.dto;
 import lombok.*;
 
 @Getter
-@Setter
 @Builder
 @AllArgsConstructor
-@NoArgsConstructor
 public class TokenDto {
 
-    private String token;
+    private final String token;
 }
