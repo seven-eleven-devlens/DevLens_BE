a5f22b5e2f56aee47015369917ff5feb35cb1be5
taekminKwon <rnjsxorals1@gmail.com>
2025-01-21 20:11:50 +0900

    refactor : 프로젝트 리팩토링

--- Admin/src/main/java/com/seveneleven/service/AdminProjectReader.java
+++ Admin/src/main/java/com/seveneleven/service/AdminProjectReader.java
@@ -1,12 +1,10 @@
-package com.seveneleven.service.adminProject;
+package com.seveneleven.service;
 
 import com.seveneleven.entity.project.Project;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.Optional;
-
 public interface AdminProjectReader {
-    Optional<Project> getProject(Long id);
+    Project getProject(Long id);
     Page<Project> findAll(Pageable pageable);
 }
