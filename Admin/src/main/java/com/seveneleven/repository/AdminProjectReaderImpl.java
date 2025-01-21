62955e825e3d4efd194c3d65535e1d6cf151f1ed
taekminKwon <rnjsxorals1@gmail.com>
2025-01-21 19:04:06 +0900

    refactor : 위치 이동

---
+++ Admin/src/main/java/com/seveneleven/repository/AdminProjectReaderImpl.java
@@ -0,0 +1,24 @@
+package com.seveneleven.repository;
+
+import com.seveneleven.entity.project.Project;
+import com.seveneleven.exception.ProjectNotFoundException;
+import com.seveneleven.service.AdminProjectReader;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AdminProjectReaderImpl implements AdminProjectReader {
+    private final AdminProjectRepository adminProjectRepository;
+    @Override
+    public Project getProject(Long id) {
+        return adminProjectRepository.findByIdAndProjectStatusCodeNot(id, Project.ProjectStatusCode.CANCELLED).orElseThrow(ProjectNotFoundException::new);
+    }
+
+    @Override
+    public Page<Project> findAll(Pageable pageable) {
+        return adminProjectRepository.findAllByProjectStatusCodeNot(pageable, Project.ProjectStatusCode.CANCELLED);
+    }
+}
