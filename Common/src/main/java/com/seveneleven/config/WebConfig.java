d2d088e97110ebfa6a636b477f59d940c34eca99
HiSeungmin <osminosmin7180@gmail.com>
2025-01-24 21:34:27 +0900

    Feat: cors 수정

--- Common/src/main/java/com/seveneleven/config/WebConfig.java
+++ Common/src/main/java/com/seveneleven/config/WebConfig.java
@@ -3,8 +3,10 @@ package com.seveneleven.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
@@ -20,4 +22,24 @@ public class WebConfig implements WebMvcConfigurer {
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(octetStreamReadMsgConverter);
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://kernel-dev-lens.vercel.app",
+                        "https://kernel-dev-lens.vercel.app",
+                        "http://localhost:3000",
+                        "https://localhost:3000",
+                        "http://www.devlens.work",
+                        "https://www.devlens.work",
+                        "http://devlens.work",
+                        "https://devlens.work",
+                        "http://api.devlens.work",
+                        "https://api.devlens.work")
+
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
 }
