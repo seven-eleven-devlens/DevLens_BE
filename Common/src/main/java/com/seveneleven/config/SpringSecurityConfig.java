fed0d8d5dfd3608c73267fbd7001a56277761524
HiSeungmin <osminosmin7180@gmail.com>
2025-01-31 14:24:39 +0900

    Feat: 관리자 - 로그인 api 추가

--- Common/src/main/java/com/seveneleven/config/SpringSecurityConfig.java
+++ Common/src/main/java/com/seveneleven/config/SpringSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.seveneleven.config;
 
-import com.seveneleven.util.security.TokenRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
@@ -14,11 +13,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 /**
  * Spring Security 설정을 정의하는 클래스
