eecf11d202f597ba1aa20395c3720f3feb192398
Eseas <8snlf9@gmail.com>
2025-02-03 15:21:17 +0900

    feat : 프로젝트 생성 시 프로젝트 기본 단계 추가

---
+++ Admin/src/main/java/com/seveneleven/service/AdminProjectStepServiceImpl.java
@@ -0,0 +1,25 @@
+package com.seveneleven.service;
+
+import com.seveneleven.common.BasicStep;
+import com.seveneleven.entity.project.Project;
+import com.seveneleven.entity.project.ProjectStep;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AdminProjectStepServiceImpl implements AdminProjectStepService {
+
+    private final ProjectStepStore projectStepStore;
+
+    @Override
+    public void createBasicStep(Project project) {
+        List<ProjectStep> basicSteps = BasicStep.createSteps(project);
+
+        for(ProjectStep step : basicSteps) {
+            projectStepStore.store(step);
+        }
+    }
+}
