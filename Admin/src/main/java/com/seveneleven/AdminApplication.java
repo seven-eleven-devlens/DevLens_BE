dcc9f26dd25b716742fe91ba0c472ab459045853
K-KY <kyuyoungk@naver.com>
2025-02-10 14:30:28 +0900

    Setting : 스웨거 api-doc 상단 링크 추가

--- Admin/src/main/java/com/seveneleven/AdminApplication.java
+++ Admin/src/main/java/com/seveneleven/AdminApplication.java
@@ -1,6 +1,7 @@
 package com.seveneleven;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +10,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 @OpenAPIDefinition(
+        info = @Info(
+                description = "Devlens Admin API 문서",
+                title = "Devlens Admin API 문서",
+                version = "1.0"
+        ),
+
         servers = {
                 @Server(description = "local Port Admin", url = "http://localhost:8443"),
                 @Server(description = "server Admin", url = "https://api.devlens.work/admin")
--- Main/src/main/java/com/seveneleven/MainApplication.java
+++ Main/src/main/java/com/seveneleven/MainApplication.java
@@ -1,6 +1,7 @@
 package com.seveneleven;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +10,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 @OpenAPIDefinition(
+        info = @Info(
+                description = "Devlens Admin API 문서",
+                title = "Devlens Admin API 문서",
+                version = "1.0"
+        ),
         servers = {
                 @Server(description = "local Port Main", url = "http://localhost:8444"),
                 @Server(description = "server Main", url = "https://api.devlens.work/main")
