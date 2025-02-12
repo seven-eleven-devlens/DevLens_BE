642dec9aa11c7e33f48516d8ff6d6d3dd4057e13
HiSeungmin <osminosmin7180@gmail.com>
2025-02-12 13:25:59 +0900

    Chore: 이메일 검증 로직 수정

--- Admin/src/main/java/com/seveneleven/member/MemberValidator.java
+++ Admin/src/main/java/com/seveneleven/member/MemberValidator.java
@@ -5,7 +5,13 @@ import com.seveneleven.member.dto.MemberDto;
 import com.seveneleven.member.repository.AdminMemberRepository;
 import com.seveneleven.response.ErrorCode;
 
+import java.util.regex.Pattern;
+
 public class MemberValidator {
+    private static final String EMAIL_REGEX = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9-]+\\.[A-Za-z]{2,}$";
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
+    private static final String PASSWORD_REGEX = "^(?=.*[0-9])(?=.*[!@#$%^&*]).+$";
+    private static final Pattern PASSWORD_PATTERN = Pattern.compile(PASSWORD_REGEX);
 
     /**
      * 함수명 : validateMember
@@ -14,6 +20,12 @@ public class MemberValidator {
      * @param memberDto 검증할 회원 요청 데이터.
      */
     public static void validateMember(AdminMemberRepository memberRepository, MemberDto.Request memberDto) {
+
+        // 로그인 ID 길이 검증
+        if (memberDto.getLoginId().length() < 4 || memberDto.getLoginId().length() > 12) {
+            throw new BusinessException(ErrorCode.INVALID_LOGIN_ID_LENGTH);
+        }
+
         // 로그인 ID 중복 확인
         if (memberRepository.existsByLoginId(memberDto.getLoginId())) {
             throw new BusinessException(ErrorCode.DUPLICATE_USER_ID);
@@ -24,8 +36,8 @@ public class MemberValidator {
             throw new BusinessException(ErrorCode.DUPLICATE_EMAIL);
         }
 
-        // **이메일 형식 검증 추가**
-        if (!isValidEmailFormat(memberDto.getEmail())) {
+        // 이메일 형식 검증 추가
+        if (!EMAIL_PATTERN.matcher(memberDto.getEmail()).matches()) {
             throw new BusinessException(ErrorCode.INVALID_EMAIL_FORMAT);
         }
 
@@ -35,15 +47,9 @@ public class MemberValidator {
         }
 
         // 비밀번호 특수문자, 숫자 포함 여부 검증
-        if (!memberDto.getPassword().matches("^(?=.*[0-9])(?=.*[!@#$%^&*]).+$")) {
+        if (!PASSWORD_PATTERN.matcher(memberDto.getPassword()).matches()) {
             throw new BusinessException(ErrorCode.INVALID_PASSWORD_FORMAT);
         }
-    }
-
-    private static boolean isValidEmailFormat(String email) {
-        // 로컬 부분에 한글 허용, 도메인 부분에 한글 및 특수 문자 비허용
-        String emailRegex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9-]+\\.[A-Za-z]{2,}$";
-        return email != null && email.matches(emailRegex);
     }
 
 }
